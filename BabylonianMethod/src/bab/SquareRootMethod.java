70435b026ab53de63d3a7dfc3870400d098db56b
Dominic Spaulding <dspaulding2@albany.edu>
2017-02-19 17:21:05 -0500

    Added variables and loaded constructor

--- BabylonianMethod/src/bab/SquareRootMethod.java
+++ BabylonianMethod/src/bab/SquareRootMethod.java
@@ -2,10 +2,18 @@ package bab;
 
 public class SquareRootMethod {
 	
+	private double numThatIsSquared;
+	private double errorPercent;
+	
 	public SquareRootMethod()
 	{
 		
 	}
 	
+	public SquareRootMethod(double num, double error)
+	{
+		numThatIsSquared = num;
+		errorPercent = error;
+	}
 	
 }
