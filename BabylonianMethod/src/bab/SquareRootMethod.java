fd12a98787c894d2c32d3a1c9c9c4a083fc11366
Dominic Spaulding <dspaulding2@albany.edu>
2017-02-26 18:08:07 -0500

    Debugged and finished the program, now to test different numbers

--- BabylonianMethod/src/bab/SquareRootMethod.java
+++ BabylonianMethod/src/bab/SquareRootMethod.java
@@ -15,20 +15,22 @@ public class SquareRootMethod {
 	
 	public static double squareRoot(double num, double initialGuess, double error)
 	{
-		errorPercent = error;
+		acceptableError = error;
 		lastGuess = initialGuess;
 		guess = initialGuess / 2;
 				
 				
-		acceptableError = (((lastGuess - guess) / lastGuess) * 100);
-		System.out.println(acceptableError);
+		errorPercent = (Math.abs((num - (lastGuess*lastGuess))) / (2*lastGuess));
 		
-		if(errorPercent <= acceptableError)
+		//System.out.println(errorPercent);
+		//System.out.println(acceptableError);
+		
+		if(errorPercent >= acceptableError)
 		{
-			System.out.println(errorPercent);
+
 			double newGuess = .5 * (lastGuess + (num / lastGuess));
-			System.out.println(errorPercent);
-			return squareRoot(num, newGuess, errorPercent);
+		
+			return squareRoot(num, newGuess, error);
 		}
 		
 		return lastGuess;
@@ -38,6 +40,6 @@ public class SquareRootMethod {
 	
 	public static void main(String args[])
 	{
-		System.out.println(squareRoot(16, 6, 20));
+		System.out.println(squareRoot(16, 6, .01));
 	}
 }
